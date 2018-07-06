1a16fc71d4dec77027083517ce8df65dbdc7ea83
David Dodson <david.dodson.001@gmail.com>
2018-07-06 08:04:26 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,15 +2,9 @@
   Please add all Javascript code to this file.
 */
 
-/**
- * create array/object with list of api keys, urls, and translation tables for variables retrieved
- * create array/object for article data to be dumped into
- * create functions to populate data from each api
- */
-
 //api set up data
 var apis = {
-/*	nyt:{
+	nyt:{
 		source:'The New York Times',
 		returnResult:'results',
 		apikey:'3a2be41461714996a25b6f9a46daa606',
@@ -31,7 +25,7 @@ var apis = {
 			abrev:'abstract',
 			impressions:'',
 		}
-	},*/
+	},
 	guardian:{
 		source:'The Guardian',
 		returnResult:'response.results',
@@ -58,226 +52,93 @@ var apis = {
 var articles = [];
 var source_menu = '<li><a class="newssource" href="#all">All</a></li>';
 var request = new XMLHttpRequest();
+var html_result = '';
 
-//$('#sources > li:nth-child(2) > a').click(function() {
-//$('#sources > li:nth-child(2)').click(function() { //doesn't work
-//$('#sources > li').click(function() { //doesn't work
-//$('#sources li').click(function() { //doesn't work
-//$('#sources').click(function() { //works
-//document.getElementById("sources").addEventListener("click", alert('displayDate'));
+$('#sources').html('');
+for(api in apis){
+	parseApi(apis[api]);
+	source_menu += '<li><a class="newssource" href="#" id="api_'+api+'">'+apis[api].source+'</a></li>'
+	$('#sources').html(source_menu);
+}
 
-/*$("a").click(function() {
-  alert("Handler for .click() called.");
-  console.log(this);
-});*/
+$('#sources').on("click", "li", function() {
+	console.log($(event.target).attr('id'));
+	parseApi(apis[api]);
+});
 
-function loopThroughApis(apis){
-	for(api in apis){
-		source_menu += '<li><a class="newssource" href="#'+api+'">'+apis[api].source+'</a></li>'
-		var requestURL = apis[api].url;
-		var request = new XMLHttpRequest();
-		var aliases = apis[api].translate;
-		request.addEventListener('load', function(){
-			var myData = JSON.parse(request.response);
-			//loop through results and push values to articles array
-			var resultsA = eval('myData.'+apis[api].returnResult);
-			for(res in resultsA){
-console.log('ddtest011b resultsA['+res+']', resultsA[res]);
-//console.log('ddtest011b', myData.results[res]);
-				var temp = {};
-				//loop through aliases and load result values into object to pass into articles array
-				for(alias in aliases){
-console.log('ddtest011c', alias);
-					//add value and key to object to be passed to articles
+
+function parseApi(api){
+console.log('api',api);
+console.log('api.url',api.url);
+	var requestURL = api.url;
+	var request = new XMLHttpRequest();
+	var aliases = api.translate;
+	request.addEventListener('load', function(){
+		var myData = JSON.parse(request.response);
+		//loop through results and push values to articles array
+		var resultsA = eval('myData.'+api.returnResult);
+		for(res in resultsA){
+console.log('resultsA[res]', resultsA[res]);
+
+/*
+
+			$('#api_'+api).bind( "click", function() {
+				parseApi(api);
+			});
+ */
+			var temp = {};
+			//loop through aliases and load result values into object to pass into articles array
+			for(alias in aliases){
+				//add value and key to object to be passed to articles
+				if(aliases[alias] == ''){
 					if(alias == 'thumb'){
-						temp[alias] = eval('myData.results[res].'+aliases[alias]);
+						temp[alias] = api['defaultthumb'];
 					}else{
-console.log(myData);
-console.log(myData.results);
-console.log(res);
-console.log(myData.results[res]);
-console.log(aliases[alias]);
-						temp[alias] = myData.results[res][aliases[alias]];
+						temp[alias] = '';
 					}
-					if(alias == 'impressions'){
-						if(aliases[alias] == ''){
-							temp[alias] = '';
-						}
-					}
+				}else if(alias == 'thumb'){
+					temp[alias] = eval('myData.results[res].'+aliases[alias]);
+				}else{
+					temp[alias] = eval('myData.'+api.returnResult+'[res].'+aliases[alias]);
 				}
-				//add value and key object to articles array
-console.log('temp', temp);
-				articles.push(temp);
 			}
-console.log('articles!!!!!!', articles);
+			//add value and key object to articles array
+			articles.push(temp);
+		}
 
-	//update the source menu options
-console.log('ddtest303');
-			$('#sources').html(source_menu);
-console.log('ddtest304');
-			populateArticles(articles, apis[api].subthumb, apis[api].defaultthumb);
-console.log('ddtest305');
-		});
+		populateArticles(articles, api.subthumb, api.defaultthumb);
+	});
 
-console.log('ddtest300');
-		request.open('GET', requestURL);
-console.log('ddtest301');
-		request.send();
-console.log('ddtest302');
-	}
+	request.open('GET', requestURL);
+console.log('requestURL',requestURL);
+	request.send();
 }
 
 function populateArticles(articles, subthumb, defaultthumb){
-console.log('ddtestAA');
-	var html_result = '';
 	for(i = 0; i < articles.length; ++i){
-console.log('ddtestAB', articles[i]);
-/*console.log('THUMB01!', articles[i].thumb);
-console.log('THUMB02!', eval(articles[i].thumb));
-console.log('THUMB03!', eval(articles[i].thumb+'[0].url'));
-console.log('THUMB04 - ', eval(articles[i].thumb+'[0]'))
-console.log('THUMB04!', eval('articles[i].thumb'+subthumb));*/
-//console.log('articles[i].thumb'+subthumb);
-//console.log(eval('articles[i].thumb'+subthumb));
 		var thumb_url = (subthumb != '') ? eval('articles[i].thumb'+subthumb) : articles[i].thumb;
-console.log('ddtest thumb1', thumb_url);
 		if(thumb_url == ''){
 			thumb_url == defaultthumb;
-console.log('ddtest thumb2', thumb_url);
 		}
-//		var thumb = 
 		html_result += '<article class="article">';
-		html_result +=  '<section class="featuredImage">';
-		html_result +=   '<img src="'+thumb_url+'" alt="'+articles[i].title+'">';
-		html_result +=  '</section>';
-		html_result +=  '<section class="articleContent">';
-		html_result +=   '<a href="#"><h3>'+articles[i].title+'</h3></a>';
-		html_result +=   '<h6>'+(articles[i].section1 != undefined ? (articles[i].section1+' ') : '');
-		html_result +=          (articles[i].section2 != undefined ? articles[i].section2 : '');
-		html_result +=          ': '+articles[i].abrev+'</h6>';
-		html_result +=  '</section>';
-		html_result +=  '<section class="impressions">'+articles[i].impressions+'</section>';
-		html_result +=  '<div class="clearfix"></div>';
+		html_result +=   '<section class="featuredImage">';
+		html_result +=     '<img src="'+thumb_url+'" alt="'+articles[i].title+'">';
+		html_result +=   '</section>';
+		html_result +=   '<section class="articleContent">';
+		html_result +=     '<a href="#"><h3>'+articles[i].title+'</h3></a>';
+		html_result +=     '<h6>'+(articles[i].section1 != undefined ? (articles[i].section1+' ') : '');
+		html_result +=            (articles[i].section2 != undefined ? articles[i].section2 : '');
+		html_result +=            ': '+articles[i].abrev+'</h6>';
+		html_result +=   '</section>';
+		html_result +=   '<section class="impressions">'+articles[i].impressions+'</section>';
+		html_result +=   '<div class="clearfix"></div>';
 		html_result += '</article>';
 	}
-console.log('html_result!!!', html_result);
 	$('#main').html(html_result);
-}
-
-loopThroughApis(apis);
-
-/***
-function loadApi(api){
-	var requestURL = api.url;
-	var request = new XMLHttpRequest();
-	request.open('GET', requestURL);
-	request.send();
-};
-
-function parseApi(){
-	request.onload = function() {
-    var x = JSON.parse(request.response);
-    console.log(x.results[0].webTitle);
-    var title = x.results[0].webTitle;
-    function addToDOM(data, element) {
-        var articles = $(".articleContent");
-        //var h3s = document.querySelectorAll("H3");
-        for (var i = 0; i < articles.length; i++) {
-            var articleTitle = $(articles[i]).find("h3")[0];
-            articleTitle.innerHTML = x.results[i].webTitle;
-        }
-    };
-    addToDOM();
-};
-
-/***
-//var requestURL = "https://content.guardianapis.com/search?api-key=cab434ec-44d1-4d51-a16e-d7c116819d03";
-var requestURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=3a2be41461714996a25b6f9a46daa606";
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.send();
-
-request.onload = function() {
-    //console.log("data", request.response);
-    var x = JSON.parse(request.response);
-//    console.log(x.response.results[0].webTitle);
-//    var title = x.response.results[0].webTitle;
-    console.log(x.results[0].webTitle);
-    var title = x.results[0].webTitle;
-    //document.getElementById("title").innerHTML = title;
-    // var source   = document.getElementById("article-template").innerHTML;
-    // var template = Handlebars.compile(source);
-    // var context = {title: "x.response.results[0].webTitle"};
-    // var html    = template(context);
-    function addToDOM(data, element) {
-        var articles = $(".articleContent");
-        //var h3s = document.querySelectorAll("H3");
-        for (var i = 0; i < articles.length; i++) {
-            var articleTitle = $(articles[i]).find("h3")[0]; 
-//            articleTitle.innerHTML = x.response.results[i].webTitle;
-            articleTitle.innerHTML = x.results[i].webTitle;
-        }
-    }
-    addToDOM();
-};
-
-/*
-request.onload = function() {
-//getNYT();
-    //console.log("data", request.response);
-    var x = JSON.parse(request.response);
-    console.log(x.response.results[0].webTitle);
-    var title = x.response.results[0].webTitle;
-    //document.getElementById("title").innerHTML = title;
-    // var source   = document.getElementById("article-template").innerHTML;
-    // var template = Handlebars.compile(source);
-    // var context = {title: "x.response.results[0].webTitle"};
-    // var html    = template(context);
-    function addToDOM(data, element) {
-        var articles = $(".articleContent");
-        //var h3s = document.querySelectorAll("H3");
-        for (var i = 0; i < articles.length; i++) {
-            var articleTitle = $(articles[i]).find("h3")[0]; 
-            articleTitle.innerHTML = x.response.results[i].webTitle;
-        }
-    }
-    addToDOM();
-}
-*/
-/***
-function getNYT(){
-	var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-	url += '?' + $.param({
-	  'api-key': "3a2be41461714996a25b6f9a46daa606"
+	$('.article').bind( "click", function() {
+		console.log('this is...');
+		console.log(this);
+		$('#popUp').show();
 	});
-	$.ajax({
-	  url: url,
-	  method: 'GET',
-	}).done(function(result) {
-	  console.log(result);
-	}).fail(function(err) {
-	  throw err;
-	});
-};
-
-/*
-function getRedditJson(ajaxRequest){
-	$.ajax({
-		method: 'GET',
-		url: 'http://www.reddit.com/r/popular/new/.json',
-		dataType: 'json',
-		success: onSuccess,
-		error: onError
-	})
 }
-*/
-/***
-function onSuccess(){
-	console.log('Woo Hoo!!!');
-};
-
-function onError(){
-	console.log('Error api not working and stuff!!!');
-};
-
-*/
+////.fields.thumbnail
